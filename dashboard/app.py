6c74ad9b5f3922db00ee2f749170333dde7a1161
Kevin Gastelum <97716634+KevinGastelum@users.noreply.github.com>
2023-10-17 20:32:46 -0700

    Update yml for Github Actions

--- dashboard/app.py
+++ dashboard/app.py
@@ -58,7 +58,7 @@ def main():
         """
     )
 
-    date = st.date_input('**Select a Date** (Data begins on date of project creation 2023/10/16)', value=pd.to_datetime('today'))  # Date Picker
+    date = st.date_input('**Select a Date** (Data begins on date of project creation 2023/10/16)', value=pd.to_datetime('today'))  # Date Picker MIGHT NEED TO CHANGE THIS TO 'yesterday'
 
     st.header(f"Top 20 Gainers on {date.strftime('%Y-%m-%d')}")
     top_gainers = fetch_data("top_gainers", date)
